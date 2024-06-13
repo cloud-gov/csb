15a83668f6772450237108dcd215c6dc0695001a
James Hochadel <james.hochadel@gsa.gov>
2024-06-13 09:36:24 -0400

    Match up AWS provider versions

--- brokerpaks/cg-smtp/terraform/bind/versions.tf
+++ brokerpaks/cg-smtp/terraform/bind/versions.tf
@@ -2,8 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "registry.terraform.io/hashicorp/aws"
-      version = "~> 3.0"
+      version = ">= 5.0"
     }
   }
-  required_version = ">= 0.13"
 }
--- brokerpaks/cg-smtp/terraform/provision/versions.tf
+++ brokerpaks/cg-smtp/terraform/provision/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "registry.terraform.io/hashicorp/aws"
-      version = "~> 3.0"
+      version = ">= 5.0"
     }
   }
 }
