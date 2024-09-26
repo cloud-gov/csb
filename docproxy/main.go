21ea77b4da5465fde334af50b66b1b9fae9e837e
James Hochadel <james.hochadel@gsa.gov>
2024-09-26 09:41:51 -0400

    xml package does not handle malformed HTML well; use x/net/html instead
    Also transformations to remove leading whitespace from header and insert stylesheet

--- docproxy/main.go
+++ docproxy/main.go
@@ -1,119 +1,76 @@
 package main
 
 import (
-	"bytes"
-	"encoding/xml"
+	"fmt"
+	"net/http"
 	"os"
 	"strings"
-	// "strings"
+
+	"golang.org/x/net/html"
 )
 
-var data = []byte(`
-<html>
-	<head>
-		<title>Hi there blah</title>
-	</head>
-	<body>
-		<h1>This is dog</h1>
-		<p>tommy</p>
-		<pre>This is
-Some content</pre>
-	</body>
-</html>
-`)
-
-// Node represents a generic element in an XML document tree.
-type Node struct {
-	XMLName xml.Name
-	Attrs   []xml.Attr `xml:",attr"`
-	Content string     `xml:",chardata"`
-	Nodes   []*Node    `xml:",any"`
-}
-
-func (n *Node) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	n.Attrs = start.Attr
-	// Avoid recursively calling UnmarshalXML by casting to an alias type.
-	type node Node
-	err := d.DecodeElement((*node)(n), &start)
-	if err != nil {
-		return err
+// walk traverses the nodes of the HTML document tree and calls f on each node. If f
+// returns true, walk stops traversing.
+func walk(n *html.Node, f func(*html.Node) bool) bool {
+	if f(n) {
+		return true
 	}
-
-	// Avoid printing xml-encoded newline and tab characters, which may be part of the
-	// content of container elements like html and div. But, leave them intact for
-	// elements that have other contents. This is important for HTML elements where
-	// formatting characters are meaningful, like <pre>.
-	content := strings.ReplaceAll(n.Content, "\n", "")
-	content = strings.ReplaceAll(content, "\t", "")
-	if len(content) == 0 {
-		n.Content = content
-	}
-
-	return nil
-}
-
-// walk traverses the nodes of an XML document tree and performs some action, specified
-// by function f, on each node. It stops traversing if f returns false.
-
-// Based on: https://stackoverflow.com/a/30257684
-func walk(nodes []*Node, f func(*Node) bool) {
-	for _, n := range nodes {
-		if f(n) {
-			walk(n.Nodes, f)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if walk(c, f) {
+			return true
 		}
 	}
+	return false
+}
+
+func insertStylesheet(n *html.Node) {
+	walk(n, func(n *html.Node) bool {
+		if n.Type == html.ElementNode && n.Data == "head" {
+			n.AppendChild(&html.Node{
+				Type: html.ElementNode,
+				Data: "link",
+				Attr: []html.Attribute{
+					{
+						Key: "rel",
+						Val: "stylesheet",
+					},
+					{
+						Key: "href",
+						Val: "styles.css",
+					},
+				},
+			})
+			return false
+		} else if n.Type == html.TextNode && n.Parent.Type == html.ElementNode && n.Parent.Data == "h1" {
+			// Trim whitespace from header, which has a leading space
+			fmt.Println("Found h1 text")
+			n.Data = strings.Trim(n.Data, " ")
+		}
+		return false
+	})
 }
 
 func main() {
-	buf := bytes.NewBuffer(data)
-	dec := xml.NewDecoder(buf)
-
-	var n = new(Node)
-	err := dec.Decode(n)
+	resp, err := http.Get("https://csb.dev.us-gov-west-1.aws-us-gov.cloud.gov")
 	if err != nil {
 		panic(err)
 	}
 
-	walk([]*Node{n}, func(n *Node) bool {
-		if n.XMLName.Local == "head" {
-			n.Nodes = append(n.Nodes, &Node{
-				XMLName: xml.Name{
-					Local: "link",
-				},
-				Attrs: []xml.Attr{
-					xml.Attr{
-						Name: xml.Name{
-							Local: "rel",
-						},
-						Value: "stylesheet",
-					},
-					xml.Attr{
-						Name: xml.Name{
-							Local: "href",
-						},
-						Value: "styles.css",
-					},
-				},
-			})
-		}
-		return true
-	})
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	insertStylesheet(doc)
 
 	file, err := os.Create("index.html")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close() // need to look up that thing about dealing with Close() errors.
-	enc := xml.NewEncoder(file)
-	enc.Indent("", "\t")
-	err = enc.Encode(n)
+
+	err = html.Render(file, doc)
 	if err != nil {
 		panic(err)
 	}
 }
-
-// find the content element
-// add an a href after
-
-// find the head element
-// add a new meta element to the end of
