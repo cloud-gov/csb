35eb5219362ec16b56a46b3ea538c5075501e9fa
James Hochadel <james.hochadel@gsa.gov>
2025-02-18 14:26:10 -0500

    Update remote state output references to account for upstream changes
    Changes in: https://github.com/cloud-gov/terraform-provision/pull/1832

--- ci/terraform/stack/apps.tf
+++ ci/terraform/stack/apps.tf
@@ -11,15 +11,15 @@ module "csb" {
   rds_username = data.terraform_remote_state.iaas.outputs.csb.rds.username
   rds_password = data.terraform_remote_state.iaas.outputs.csb.rds.password
 
-  ecr_access_key_id                = data.terraform_remote_state.iaas.outputs.csb.ecr_user.access_key_id_curr
-  ecr_secret_access_key            = data.terraform_remote_state.iaas.outputs.csb.ecr_user.secret_access_key_curr
+  ecr_access_key_id                = data.terraform_remote_state.iaas.outputs.csb.iam.ecr.access_key_id_curr
+  ecr_secret_access_key            = data.terraform_remote_state.iaas.outputs.csb.iam.ecr.secret_access_key_curr
   instances                        = 1
   aws_ses_default_zone             = var.csb_aws_ses_default_zone
-  aws_access_key_id_govcloud       = data.terraform_remote_state.iaas.outputs.csb.broker_user.access_key_id_curr
-  aws_secret_access_key_govcloud   = data.terraform_remote_state.iaas.outputs.csb.broker_user.secret_access_key_curr
+  aws_access_key_id_govcloud       = data.terraform_remote_state.iaas.outputs.csb.iam.csb.access_key_id_curr
+  aws_secret_access_key_govcloud   = data.terraform_remote_state.iaas.outputs.csb.iam.csb.secret_access_key_curr
   aws_region_govcloud              = var.csb_aws_region_govcloud
-  aws_access_key_id_commercial     = data.terraform_remote_state.external.outputs.csb.broker_user.access_key_id_curr
-  aws_secret_access_key_commercial = data.terraform_remote_state.external.outputs.csb.broker_user.secret_access_key_curr
+  aws_access_key_id_commercial     = data.terraform_remote_state.external.outputs.csb.iam.csb.access_key_id_curr
+  aws_secret_access_key_commercial = data.terraform_remote_state.external.outputs.csb.iam.csb.secret_access_key_curr
   aws_region_commercial            = var.csb_aws_region_commercial
 
   email_notification_topic_arn = data.terraform_remote_state.iaas.outputs.csb.notification_topics.email_notification_topic_arn
--- ci/terraform/stack/providers.tf
+++ ci/terraform/stack/providers.tf
@@ -4,8 +4,8 @@ terraform {
 }
 
 provider "aws" {
-  access_key = data.terraform_remote_state.iaas.outputs.csb.concourse_user.access_key_id_curr
-  secret_key = data.terraform_remote_state.iaas.outputs.csb.concourse_user.secret_access_key_curr
+  access_key = data.terraform_remote_state.iaas.outputs.csb.iam.concourse.access_key_id_curr
+  secret_key = data.terraform_remote_state.iaas.outputs.csb.iam.concourse.secret_access_key_curr
   region     = var.csb_aws_region_govcloud
 }
 
