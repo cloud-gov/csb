b65c2b6f9418963cfc025742b2ac762b56e98ccd
James Hochadel <james.hochadel@gsa.gov>
2025-02-19 16:50:31 -0500

    Revert lines - I didn't actually change these outputs

--- ci/terraform/stack/apps.tf
+++ ci/terraform/stack/apps.tf
@@ -18,8 +18,8 @@ module "csb" {
   aws_access_key_id_govcloud       = data.terraform_remote_state.iaas.outputs.csb.iam.csb.access_key_id_curr
   aws_secret_access_key_govcloud   = data.terraform_remote_state.iaas.outputs.csb.iam.csb.secret_access_key_curr
   aws_region_govcloud              = var.csb_aws_region_govcloud
-  aws_access_key_id_commercial     = data.terraform_remote_state.external.outputs.csb.iam.csb.access_key_id_curr
-  aws_secret_access_key_commercial = data.terraform_remote_state.external.outputs.csb.iam.csb.secret_access_key_curr
+  aws_access_key_id_commercial     = data.terraform_remote_state.external.outputs.csb.broker_user.access_key_id_curr
+  aws_secret_access_key_commercial = data.terraform_remote_state.external.outputs.csb.broker_user.secret_access_key_curr
   aws_region_commercial            = var.csb_aws_region_commercial
 
   email_notification_topic_arn = data.terraform_remote_state.iaas.outputs.csb.notification_topics.email_notification_topic_arn
