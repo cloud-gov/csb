8babe52f7e1990d42b66f53814c6e6dba5a61e92
Mark Boyd <mark.boyd@gsa.gov>
2026-03-18 17:02:40 -0400

    Fix bind behavior when feedback notifications are enabled (#39)
    * minor improvements to testing scripts
    
    * minor improvement to test script
    
    * update notification_webhook -> notification_email & update logic for handling value for notification_email
    
    * rename notification_webhook -> notification_email
    
    * add documentation on testing CSB locally
    
    * update acceptance tests to set notification email when binding services with feedback notifications enabled

--- DEVELOPING.md
+++ DEVELOPING.md
@@ -1,4 +1,4 @@
-# Developing the Cloud Service Broker
+# Developing the Cloud Service Broker (CSB)
 
 ## Prerequisites
 
@@ -10,23 +10,25 @@ cd cloud-service-broker
 go install
 ```
 
-## Testing the brokerpak build
+## Testing the CSB build
 
 Copy `secrets.env.example` to `secrets.env` and update the missing values.
 
-To do a one-time build of the broker:
+To do a one-time build of the CSB:
 
 ```shell
 make build
 ```
 
-To watch local files and re-build the broker as they change:
+To watch local files and re-build the CSB as they change:
 
 ```shell
 make watch
 ```
 
 ## Testing Terraform code
+
+> WARNING: Following these steps will actually provision AWS resources
 
 ## SES brokerpak
 
@@ -39,3 +41,55 @@ make watch
 1. Run `terraform init`
 1. Copy `terraform.tfvars-template` to `terraform.tfvars` and add missing values
 1. Run `terraform plan` and `terraform apply` to verify whether the Terraform code works as expected
+
+## Testing brokerpaks locally
+
+You can run the Cloud Service Broker (CSB) locally to validate that your brokerpak works
+as expected when packaged into the CSB.
+
+### Testing SES brokerpak
+
+> WARNING: Following these steps will actually provision AWS resources
+
+1. Copy `secrets.env.example` to `secrets.env` and update the missing values
+1. Run the CSB locally in "watch" mode, which will rebuild and restart the CSB as you change files:
+
+    ```shell
+    make watch
+    ```
+
+1. Create `brokerpaks/aws-ses/client/clientconfig.yml`:
+
+    ```yaml
+    api:
+        user: <broker-username>
+        password: <broker-password>
+        port: 8081
+    ```
+
+    where
+
+    - `broker-username` matches `SECURITY_USER_NAME` from `secrets.env`
+    - `broker-password` matches `SECURITY_USER_PASSWORD` from `secrets.env`
+
+1. From the `brokerpaks/aws-ses/client` directory, run `up.sh`:
+
+    ```shell
+    cd brokerpaks/aws-ses/client
+    ./bin/up.sh your-email@agency.gov .
+    ```
+
+    You can optionally specify additional parameters to the service creation like so:
+
+    ```shell
+    /bin/up.sh your-email@agency.gov . '{"enable_feedback_notifications": true}'
+    ```
+
+1. Verify that the `up.sh` scripts returns without an error and that `brokerpaks/aws-ses/client/credentials.json` contains bound credentials for the created service
+1. Deprovision the created resources:
+
+    ```shell
+    ./bin/down.sh .
+    ```
+
+1. Verify that the `down.sh` completes successfully
--- brokerpaks/aws-ses/client/bin/up.sh
+++ brokerpaks/aws-ses/client/bin/up.sh
@@ -2,43 +2,45 @@
 set -eo pipefail
 
 if [ "$#" -lt 2 ]; then
-  printf "Usage:\n\n\t\$./up.sh recipient-email@gsa.gov /path/to/workdir\n\n"
+  printf "Usage:\n\n\t\$./up.sh recipient-email@gsa.gov /path/to/workdir {\"extra_param\": \"value\"}\n\n"
   exit 1
 fi
 
 recipient=$1
 workdir=$2
+extra_params=${3:-"{}"}
 
 csb=cloud-service-broker
 
 # Instance IDs must be unique, so generate a new one
-instanceid=$(uuidgen | tr "[A-Z]" "[a-z]")
+instanceid=$(uuidgen | tr '[:upper:]' '[:lower:]')
 echo "Instance ID: $instanceid"
 
 # Start provisioning
-$csb client provision --config clientconfig.yml --planid 35ffb84b-a898-442e-b5f9-0a6a5229827d --serviceid 260f2ead-b9e9-48b5-9a01-6e3097208ad7 --instanceid $instanceid --params "{\"dmarc_report_aggregate_recipients\": \"mailto:${recipient}\", \"dmarc_report_failure_recipients\": \"${recipient}\"}"
+params=$(jq -n --arg recipient "$recipient" --argjson extraparams "$extra_params" '{"admin_email": $recipient} + $extraparams')
+$csb client provision --config clientconfig.yml --planid 35ffb84b-a898-442e-b5f9-0a6a5229827d --serviceid 260f2ead-b9e9-48b5-9a01-6e3097208ad7 --instanceid "$instanceid" --params "$params"
 
 # Wait on provisioning to finish
 state=""
 while [[ "$state" != "succeeded" ]]; do
 	sleep 10
-	state=$($csb client --config clientconfig.yml last --instanceid $instanceid | jq -r '.response.state')
+	state=$($csb client --config clientconfig.yml last --instanceid "$instanceid" | jq -r '.response.state')
 	echo "State: $state"
 done
 
 touch "$workdir/instances.txt"
-echo $instanceid >> "$workdir/instances.txt"
+echo "$instanceid" >> "$workdir/instances.txt"
 
 # Let the broker settle
 sleep 1
 
 # Binding IDs must be unique, so generate a new one
-bindingid=$(uuidgen | tr "[A-Z]" "[a-z]")
+bindingid=$(uuidgen | tr '[:upper:]' '[:lower:]')
 echo "Binding ID: $bindingid"
 touch "$workdir/bindings.txt"
 echo "$instanceid $bindingid" >> "$workdir/bindings.txt"
 
 # Update smtp-client with new credentials
-$csb client bind --config clientconfig.yml --planid 35ffb84b-a898-442e-b5f9-0a6a5229827d --serviceid 260f2ead-b9e9-48b5-9a01-6e3097208ad7 --instanceid $instanceid --bindingid $bindingid | jq '.response.credentials' > "$workdir/credentials.json"
+$csb client bind --config clientconfig.yml --planid 35ffb84b-a898-442e-b5f9-0a6a5229827d --serviceid 260f2ead-b9e9-48b5-9a01-6e3097208ad7 --instanceid "$instanceid" --bindingid "$bindingid" | jq '.response.credentials' > "$workdir/credentials.json"
 
 echo "Done. Credentials saved to credentials.json for use with the client. GUIDs saved to instances.txt and bindings.txt. Deprovision later with down.sh."
