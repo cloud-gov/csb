cfb5950c3d4e343ba822df75ebe50b5cc4933da5
Mark Boyd <mark.boyd@gsa.gov>
2026-09-23 16:08:38 -0400

    add missing terraform variable

--- ci/terraform/stack/tests.tf
+++ ci/terraform/stack/tests.tf
@@ -1,7 +1,8 @@
 module "ses_broker_test_user" {
   source = "../module/test-credentials"
 
-  org_name              = var.ses_broker_tests_org_name
-  space_name            = var.ses_broker_tests_space_name
-  deployer_service_name = var.ses_broker_tests_deployer_service_name
+  org_name                  = var.ses_broker_tests_org_name
+  space_name                = var.ses_broker_tests_space_name
+  deployer_service_name     = var.ses_broker_tests_deployer_service_name
+  deployer_service_key_name = var.ses_broker_tests_deployer_service_key_name
 }
