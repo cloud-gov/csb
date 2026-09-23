fd64d0660837206dfb9fd0c536c4bb427b6c27c5
Mark Boyd <mark.boyd@gsa.gov>
2026-09-23 16:45:54 -0400

    Create test user for broker tests (#51)
    * move terraform for CSB app into csb folder & create terraform module to provision test credentials
    
    * add variable for test user credentials service key name
    
    * add terraform variables for SES broker test users and test service key names to pipeline
    
    * update login helper function to read credentials from service key, if name of service key is provided via environment variables
    
    * Update ci/terraform/stack/tests.tf
    
    Co-authored-by: Peter Burkholder <pburkholder@pobox.com>
    Signed-off-by: Mark Boyd <markdavidboyd@gmail.com>
    
    * add missing terraform variable
    
    * fix logic in CI script
    
    * fix name of space terraform resource
    
    ---------
    
    Signed-off-by: Mark Boyd <markdavidboyd@gmail.com>
    Co-authored-by: Peter Burkholder <pburkholder@pobox.com>

--- ci/ci-utils.sh
+++ ci/ci-utils.sh
@@ -3,7 +3,22 @@
 login() {
   cf api "$CF_API_URL"
   set +x
+
+  if [[ -z "$CF_USERNAME" || -z "$CF_PASSWORD" ]]; then
+    if [[ -n "$CF_USER_SERVICE_NAME" && -n "$CF_USER_SERVICE_KEY_NAME" ]]; then
+      CF_USER_SERVICE_KEY_GUID=$(cf service-key "$CF_USER_SERVICE_NAME" "$CF_USER_SERVICE_KEY_NAME" --guid)
+      CF_USERNAME=$(cf curl "/v3/service_credential_bindings/$CF_USER_SERVICE_KEY_GUID/details" | jq -r '.credentials.username')
+      CF_PASSWORD=$(cf curl "/v3/service_credential_bindings/$CF_USER_SERVICE_KEY_GUID/details" | jq -r '.credentials.password')
+    fi
+  fi
+
+  if [[ -z "$CF_USERNAME" || -z "$CF_PASSWORD" ]]; then
+    echo "missing required credentials for login"
+    exit 1
+  fi
+
   cf auth "$CF_USERNAME" "$CF_PASSWORD"
+
   set -x
   cf target -o "$CF_ORGANIZATION" -s "$CF_SPACE"
 }
---
+++ ci/terraform/module/test-credentials/credentials.tf
@@ -0,0 +1,28 @@
+data "cloudfoundry_org" "tests_org" {
+  name = var.org_name
+}
+
+data "cloudfoundry_space" "tests_space" {
+  name = var.space_name
+  org  = data.cloudfoundry_org.tests_org.id
+}
+
+data "cloudfoundry_service_plans" "space_deployer_account_plan" {
+  service_offering_name = "cloud-gov-service-account"
+  name                  = "space-deployer"
+  service_broker_name   = "uaa-credentials-broker"
+}
+
+resource "cloudfoundry_service_instance" "test_user" {
+  name  = var.deployer_service_name
+  space = data.cloudfoundry_space.tests_space.id
+  type  = "managed"
+
+  service_plan = data.cloudfoundry_service_plans.space_deployer_account_plan.service_plans[0].id
+}
+
+resource "cloudfoundry_service_credential_binding" "test_user_key" {
+  type             = "key"
+  name             = var.deployer_service_key_name
+  service_instance = cloudfoundry_service_instance.test_user.id
+}
